82a48a140f5c333a7b12827265b49e7a16691b80
Ashu2208 <aashish.patel@ymail.com>
2022-10-18 22:52:44 +0530

    first commit

---
+++ src/redux/Constants.js
@@ -0,0 +1,16 @@
+export const BASE_URL = "https://api.unsplash.com/";
+export const ACCESS_KEY = "dqEI8J5GShfPzRPA-UaJJHNTlDXTQKfFDhaOPIpF-i0";
+export const SECRET_KEY = "MMdVBqvG4nyZsJGsaYx_iNRwhoG-bULWKTSM6ZN7Pyg";
+
+
+
+
+
+
+// Common Status Codes	Description
+// 200 - OK	Everything worked as expected
+// 400 - Bad Request	The request was unacceptable, often due to missing a required parameter
+// 401 - Unauthorized	Invalid Access Token
+// 403 - Forbidden	Missing permissions to perform request
+// 404 - Not Found	The requested resource doesn’t exist
+// 500, 503 Something went wrong on our end
